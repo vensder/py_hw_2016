bd59ca688ec34e27ae16dffd1055133d0b844b62
vensder <vensder@gmail.com>
2016-03-04 16:00:16 +0300

    hw 5 added

--- hw5.py
+++ hw5.py
@@ -4,45 +4,63 @@
 #  2016_python.py
 #  
 #  Student Dmitry Makarov <vensder@gmail.com>
-
 # 5
-
 # 1. задание со студентами делим на несколько
 # модулей с функциями + main.py
-
 # 2. срезы и индексы с использованием try-except
-
-# 3. в задании с pickle тоже try-except
+# 3. в задании с pickle тоже try-except # см. второй файл.
 
 #1
-
 import students
 
 students.print_students()
 #print(students.quantity)
 
 #2
-print(
-'''Введите порядковый номер студента.
-Номер должен быть от 1 до''', students.quantity)
+def input_number():
+    print(
+    u'''Введите порядковый номер студента.
+    Номер должен быть от 1 до''', students.quantity)
+    try:
+        index = int(input()) - 1
+        print(u'\nВы ввели номер:', index+1)
+        return index
+    except ValueError:
+        print(u'Ввели какую-то фигню')
+        quit()
 
-index = int(input()) - 1
+def try_output_student(index):
+    try:
+        print(u'\nСтудент под номером ' + str(index+1) + ':', students.students[index])
+        return True
+    except IndexError:
+        print(u'Порядковый номер за пределами диапазона, извините...')
+        input_number()
+        return False
 
-if not (0 <= index < students.quantity):
-    print('Порядковый номер за пределами диапазона, извините...')
-    quit()
+index = input_number()
+while not try_output_student(index):
+    index = input_number()
 
-print('\nВы ввели номер:', index+1)
-print('\nСтудент под номером ' + str(index+1) + ':', students.students[index])
 
 #3
-print('\nВведите диапазон номеров студентов в формате "x-y", где x - начало, а y - конец диапазона')
+print(u'\nВведите диапазон номеров студентов в формате "x-y", где x - начало, а y - конец диапазона')
 
 students_range = input().split('-')
-begin = int(students_range[0])
-end = int(students_range[1])
 
-print('\nДиапазон студентов(срез):')
+try:
+    begin = int(students_range[0])
+except ValueError:
+    print(u'Ввели неправильное значение начала диапазона')
+    quit()
+
+try:
+    end = int(students_range[1])
+except ValueError:
+    print(u'Ввели неправильное значение конца диапазона')
+    quit()
+
+print(u'\nДиапазон студентов(срез):')
 print(students.students[begin-1:end])
 
 #4
@@ -51,9 +69,9 @@ for s in students.students:
     if s.find('р') != -1:
         i+=1
 
-input('Нажмите Enter для продолжения')
+input(u'Нажмите Enter для продолжения')
 
-print('\nКоличество студентов с буквой "р":', i)
+print(u'\nКоличество студентов с буквой "р":', i)
 
 #5
 # Split each element of list to list whith Surname, Name
@@ -75,7 +93,7 @@ for i in range(1,len(list0),2):
             list0[i].append(students.students[j][0])
 
 #print name and surnames
-print('\nГруппы студентов по именам:')
+print(u'\nГруппы студентов по именам:')
 for i in range(len(list0)):
     print(list0[i])
 
